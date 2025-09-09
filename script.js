a0b3df19ada6a81eb9156cae6805c81d0da8a11b
paragoncapstone <jebulanaxlejohn29@gmail.com>
2025-09-10 02:05:22 +0800

    changes

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
     navLinks.classList.toggle("active");
 
-    // Toggle burger icon
+ 
     if (navLinks.classList.contains("active")) {
       burger.classList.replace("bi-list", "bi-x");
     } else {
@@ -12,7 +12,7 @@
     }
   }
 
-  // (Optional) Close menu when a link is clicked
+ 
   document.querySelectorAll(".nav-links a").forEach(link => {
     link.addEventListener("click", () => {
       document.querySelector(".nav-links").classList.remove("active");
